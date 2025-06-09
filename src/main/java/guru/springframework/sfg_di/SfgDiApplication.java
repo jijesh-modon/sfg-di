c32c9865fa476774591c9177835d39e0ffa512e0
Jijesh Modon <jijeshmodon8@gmail.com>
2025-06-10 01:12:57 +0530

    Spring context initialization

---
+++ src/main/java/guru/springframework/sfg_di/SfgDiApplication.java
@@ -0,0 +1,18 @@
+package guru.springframework.sfg_di;
+
+import guru.springframework.sfg_di.controllers.MyController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SfgDiApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) context.getBean("myController");
+		System.out.println(myController.sayHello());
+	}
+
+}
