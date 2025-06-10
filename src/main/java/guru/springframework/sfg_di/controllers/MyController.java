090c7532c00467d35c972fbd54a6a9347cd511a1
Jijesh Modon <jijeshmodon8@gmail.com>
2025-06-10 17:05:43 +0530

    Spring Dependency Injection with PRIMARY

--- src/main/java/guru/springframework/sfg_di/controllers/MyController.java
+++ src/main/java/guru/springframework/sfg_di/controllers/MyController.java
@@ -1,9 +1,20 @@
 package guru.springframework.sfg_di.controllers;
 
+import guru.springframework.sfg_di.services.GreetingService;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
+
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting() {
+        return greetingService.sayGreeting();
+    }
 
     public String sayHello() {
         System.out.println("Hey There!");
